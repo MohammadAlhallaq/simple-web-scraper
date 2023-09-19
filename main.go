750a1a6f52539caa1391c6baa7c048058ed13206
Mohammad_Alhallaq <mohammad.alhallaq1@gmail.com>
2023-09-19 23:54:13 +0300

    wip

--- main.go
+++ main.go
@@ -1,26 +1,77 @@
 package main
 
 import (
+	"bytes"
+	"encoding/csv"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
+	"log"
+	"net/url"
+	"os"
 )
 
-func main() {
-	c := colly.NewCollector(colly.AllowedDomains(
-		"my.majarrastg.com",
-	))
+type visitUrl struct {
+	domain   string
+	fullPath string
+}
 
-	c.OnHTML("a[href]", func(element *colly.HTMLElement) {
-		err := element.Request.Visit(element.Attr("href"))
+func main() {
+	visitingUrl := &visitUrl{
+		domain:   "my.majarra.com",
+		fullPath: "https://my.majarra.com",
+	}
+
+	var uniqueHrefs []string
+
+	c := colly.NewCollector(colly.AllowedDomains(visitingUrl.domain))
+	c.AllowURLRevisit = true
+
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		reader := bytes.NewReader(e.Response.Body)
+
+		doc, err := goquery.NewDocumentFromReader(reader)
 		if err != nil {
-			panic(err.Error())
+			fmt.Println("Error parsing the body:", err)
+			return
 		}
+
+		doc.Find("a").Each(func(i int, selection *goquery.Selection) {
+			link, _ := selection.Attr("href")
+			parsedURL, err := url.Parse(link)
+			if err != nil {
+				fmt.Println("Error parsing URL:", err)
+				return
+			}
+			if parsedURL.Hostname() == "" && parsedURL.Path != "" {
+				link = visitingUrl.fullPath + parsedURL.Path
+			}
+			uniqueHrefs = append(uniqueHrefs, link)
+
+			err = os.MkdirAll("Links", os.ModePerm)
+			file, err := os.Create("./links/home.csv")
+			defer file.Close()
+
+			if err != nil {
+				return
+			}
+
+			writer := csv.NewWriter(file)
+			defer writer.Flush()
+
+			for _, record := range uniqueHrefs {
+				if err := writer.Write([]string{record}); err != nil {
+					log.Fatalln("error writing record to file", err)
+				}
+			}
+		})
 	})
+
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	err := c.Visit("https://my.majarrastg.com/")
+	err := c.Visit(visitingUrl.fullPath)
 	if err != nil {
 		panic(err.Error())
 	}
